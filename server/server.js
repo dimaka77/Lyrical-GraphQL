b198471992d2759b3ec469da50b27ee07389cc32
DR <dimaka77@gmail.com>
2018-05-20 21:44:44 -0700

    Added: initial setup

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const schema = require('./schema/schema');
 const app = express();
 
 // Replace with your mongoLab URI
-const MONGO_URI = '';
+const MONGO_URI = 'mongodb://dimaka77:Emilia31#18141209@ds217310.mlab.com:17310/graphqldb';
 if (!MONGO_URI) {
   throw new Error('You must provide a MongoLab URI');
 }
